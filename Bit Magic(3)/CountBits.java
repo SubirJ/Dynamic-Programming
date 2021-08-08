86e1b7d78bc899749cc913731e7b04ceb3311931
SubirJ <subirsjadhav.14@gmail.com>
2021-08-08 23:20:42 +0530

    Count number of active bits of a number

---
+++ Bit Magic(3)/CountBits.java
@@ -0,0 +1,27 @@
+/*
+Count the number of set bits of a given number.
+
+number : 7 -> 0111
+o/p : 3 (As 3 bits are set
+
+number : 13 -> 1101
+o/p : 3
+
+number : 5 -> 101
+o/p : 2
+*/
+
+import java.util.*;
+
+class CountBits{
+	public static void main(String[] args){
+		int n = 0;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(n>0){
+			System.out.println("Enter the number : ");
+			n = sc.nextInt();
+		}
+	}
+}
