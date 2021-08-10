2aaa89d407ac5ee0283daad8748bded9b169b6c2
SubirJ <subirsjadhav.14@gmail.com>
2021-08-10 23:27:12 +0530

    Program to count number of set bits

--- Bit Magic(3)/CountBits.java
+++ Bit Magic(3)/CountBits.java
@@ -14,14 +14,37 @@ o/p : 2
 import java.util.*;
 
 class CountBits{
+	
+	//Naive Solution
+	//if we have 5(0101), it has 2 set bits so we will check if first bit is set or not and then discard the bit 
+	//by moving it to the right. We will check if first bit is set or not by n/2.
+	//We will continue this process till the number n becomes zero, if all bits become 000... then number becomes zero.
+	//For every set bit we will increment our result.
+	public static int countSet(int x){
+		
+		int res = 0;
+		
+		while(x > 0){
+			res = res + (x & 1); // x & 1, if its odd returns 1 else so our result will increment if its odd
+			//Or we can check,
+			//if(x % 2 != 0)
+			//	res++;
+			x = x/2; //to remove the last bit, or we can do x = x >> 1 to remove last bit
+		}
+		
+		return res;
+	}
+	
 	public static void main(String[] args){
-		int n = 0;
+		int n = 1;
 		
 		Scanner sc = new Scanner(System.in);
 		
 		while(n>0){
 			System.out.println("Enter the number : ");
 			n = sc.nextInt();
+			
+			System.out.println("The number of set bits are(naive solution : "+countSet(n));
 		}
 	}
 }
